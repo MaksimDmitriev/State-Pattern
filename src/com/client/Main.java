122b1a97cd3f35dfca1e45cd4d81228f2b5087cd
Maksim Dmitriev <maksim.a.dmitriev@gmail.com>
2017-06-27 09:54:01 +0300

    Add a TODO

--- src/com/client/Main.java
+++ src/com/client/Main.java
@@ -4,6 +4,7 @@ import com.api.GitFileManager;
 
 class Main {
 
+    // TODO: automated tests
     public static void main(String[] args) {
         GitFileManager gitFileManager = GitFileManager.getInstance();
         gitFileManager.addFile();
